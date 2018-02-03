ff5fa430037c79a0cc675ea4ffa06c383845e6b9
aditya314 <anksshady@gmail.com>
2018-02-03 11:10:14 +0530

    Activity navigation for bottom activity by adding 4 more activities

---
+++ app/src/main/java/com/example/android/memophile/UploadActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.memophile;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.example.android.memophile.Utils.BottomNavigationViewHelper;
+
+
+/**
+ * Created by aditya314 on 2/3/2018.
+ */
+
+public class UploadActivity extends AppCompatActivity{
+    private static final int ACTIVITY_NUM = 2;
+
+    private Context mContext = UploadActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
--- app/src/main/java/com/example/android/memophile/Utils/BottomNavigationViewHelper.java
+++ app/src/main/java/com/example/android/memophile/Utils/BottomNavigationViewHelper.java
@@ -1,5 +1,18 @@
 package com.example.android.memophile.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+
+import com.example.android.memophile.HomeActivity;
+import com.example.android.memophile.LikeActivity;
+import com.example.android.memophile.ProfileActivity;
+import com.example.android.memophile.R;
+
+import com.example.android.memophile.SearchActivity;
+import com.example.android.memophile.UploadActivity;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 /**
@@ -12,5 +25,42 @@ public class BottomNavigationViewHelper {
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.ic_home:
+                        Intent intent1 = new Intent(context, HomeActivity.class);//ACTIVITY_NUM = 0
+                        context.startActivity(intent1);
+                        break;
+
+                    case R.id.ic_search:
+                        Intent intent2  = new Intent(context, SearchActivity.class);//ACTIVITY_NUM = 1
+                        context.startActivity(intent2);
+                        break;
+
+                    case R.id.ic_upload:
+                        Intent intent3 = new Intent(context, UploadActivity.class);//ACTIVITY_NUM = 2
+                        context.startActivity(intent3);
+                        break;
+
+                    case R.id.ic_alert:
+                        Intent intent4 = new Intent(context, LikeActivity.class);//ACTIVITY_NUM = 3
+                        context.startActivity(intent4);
+                        break;
+
+                    case R.id.ic_profile:
+                        Intent intent5 = new Intent(context, ProfileActivity.class);//ACTIVITY_NUM = 4
+                        context.startActivity(intent5);
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 }

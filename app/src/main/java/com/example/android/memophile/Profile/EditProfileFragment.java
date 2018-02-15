375b4961197d93926470aa81f3d9070ff1766bba
aditya314 <anksshady@gmail.com>
2018-02-15 12:46:14 +0530

    Setting up universal image loader

--- app/src/main/java/com/example/android/memophile/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/android/memophile/Profile/EditProfileFragment.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.android.memophile.R;
+import com.example.android.memophile.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by aditya314 on 2/13/2018.
@@ -15,11 +18,28 @@ import com.example.android.memophile.R;
 
 public class EditProfileFragment extends Fragment {
 
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        String imgURL = "www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }

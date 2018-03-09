b52d220c3a2a83e00cb29bad15b2d23d56ab6646
aditya314 <anksshady@gmail.com>
2018-03-09 15:42:44 +0530

    Organised login and register activity

--- app/src/main/java/com/example/android/memophile/Like/LikeActivity.java
+++ app/src/main/java/com/example/android/memophile/Like/LikeActivity.java
@@ -24,8 +24,8 @@ public class LikeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        //setupBottomNavigationView();
+        setContentView(R.layout.activity_home);
+        setupBottomNavigationView();
     }
 
     /**
---
+++ app/src/main/java/com/example/android/memophile/Login/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.memophile.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.memophile.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

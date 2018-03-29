1bdb5622265b8d86ca46250f4f90de1d82b643f1
aditya314 <anksshady@gmail.com>
2018-03-29 10:26:05 +0530

    Verified permission to read and write external storage

--- app/src/main/java/com/example/android/memophile/Upload/UploadActivity.java
+++ app/src/main/java/com/example/android/memophile/Upload/UploadActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.memophile.Upload;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.android.memophile.R;
+import com.example.android.memophile.Utils.Permissions;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.example.android.memophile.Utils.BottomNavigationViewHelper;
 
@@ -17,7 +20,9 @@ import com.example.android.memophile.Utils.BottomNavigationViewHelper;
  */
 
 public class UploadActivity extends AppCompatActivity{
+
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     private Context mContext = UploadActivity.this;
 
@@ -25,14 +30,62 @@ public class UploadActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+
+        }else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+        //setupBottomNavigationView();
+    }
+
+    public void verifyPermissions(String[] permissions){
+
+        ActivityCompat.requestPermissions(
+                UploadActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions){
+
+        for(int i = 0; i< permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check a single permission is it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(UploadActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
     /**
      * BottomNavigationView setup
      */
     private void setupBottomNavigationView(){
-        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
         BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
         Menu menu = bottomNavigationViewEx.getMenu();
---
+++ app/src/main/java/com/example/android/memophile/Utils/Permissions.java
@@ -0,0 +1,15 @@
+package com.example.android.memophile.Utils;
+
+import android.Manifest;
+
+/**
+ * Created by aditya314 on 3/29/2018.
+ */
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+}

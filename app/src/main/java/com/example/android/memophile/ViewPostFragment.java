719ef7d132b2d6effafd2796d7c4eebf20acaeaa
aditya314 <anksshady@gmail.com>
2018-03-31 16:48:20 +0530

    retrieving meme from bundle

--- app/src/main/java/com/example/android/memophile/ViewPostFragment.java
+++ app/src/main/java/com/example/android/memophile/ViewPostFragment.java
@@ -4,8 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.memophile.Utils.BottomNavigationViewHelper;
+import com.example.android.memophile.Utils.Photo;
+import com.example.android.memophile.Utils.UniversalImageLoader;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 /**
  * Created by aditya314 on 3/31/2018.
@@ -13,11 +22,87 @@ import android.view.ViewGroup;
 
 public class ViewPostFragment extends Fragment {
 
+    public ViewPostFragment(){
+        super();
+        setArguments(new Bundle());
+    }
+    //widgets
+    private ImageView mPostImage;
+    private BottomNavigationViewEx bottomNavigationView;
+    private TextView mBackLabel, mCaption, mUsername, mTimestamp;
+    private ImageView mBackArrow, mEllipses, mHeartRed, mHeartWhite, mProfileImage;
+
+
+    //vars
+    private Photo mPhoto;
+    private int mActivityNumber = 0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_post, container, false);
+        mPostImage = (ImageView) view.findViewById(R.id.post_image);
+        bottomNavigationView = (BottomNavigationViewEx) view.findViewById(R.id.bottomNavViewBar);
+        mBackArrow = (ImageView) view.findViewById(R.id.backArrow);
+        mBackLabel = (TextView) view.findViewById(R.id.tvBackLabel);
+        mCaption = (TextView) view.findViewById(R.id.image_caption);
+        mUsername = (TextView) view.findViewById(R.id.username);
+        mTimestamp = (TextView) view.findViewById(R.id.image_time_posted);
+        mEllipses = (ImageView) view.findViewById(R.id.ivEllipses);
+        mHeartRed = (ImageView) view.findViewById(R.id.image_heart_red);
+        mHeartWhite = (ImageView) view.findViewById(R.id.image_heart);
+        mProfileImage = (ImageView) view.findViewById(R.id.profile_photo);
+
+        try{
+            mPhoto = getPhotoFromBundle();
+            UniversalImageLoader.setImage(mPhoto.getImage_path(), mPostImage, null, "");
+            mActivityNumber = getActivityNumFromBundle();
+
+        }catch (NullPointerException e){
+
+        }
+
+        setupBottomNavigationView();
 
         return view;
+    }
+
+    /**
+     * retrieve the activity number from the incoming bundle from profileActivity interface
+     * @return
+     */
+    private int getActivityNumFromBundle(){
+
+        Bundle bundle = this.getArguments();
+        if(bundle != null) {
+            return bundle.getInt(getString(R.string.activity_number));
+        }else{
+            return 0;
+        }
+    }
+
+    /**
+     * retrieve the photo from the incoming bundle from profileActivity interface
+     * @return
+     */
+    private Photo getPhotoFromBundle(){
+
+        Bundle bundle = this.getArguments();
+        if(bundle != null) {
+            return bundle.getParcelable(getString(R.string.photo));
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(getActivity() ,bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(mActivityNumber);
+        menuItem.setChecked(true);
     }
 }
